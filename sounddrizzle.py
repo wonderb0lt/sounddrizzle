b5212217e224e7c2c86617ceb7cac164596a824d
Patrick Stegmann <code@patrick-stegmann.de>
2013-06-25 13:00:59 +0200

    PEP8'ed the sole file

--- sounddrizzle.py
+++ sounddrizzle.py
@@ -20,56 +20,59 @@ log = logging.getLogger('SoundDrizzle')
 
 
 def main(args):
-	set_loglevel(args)
-	client = soundcloud.Client(client_id='7b4b979cb9f47830a7bb2441f6d784c3')
-	track = get_track_by_url(client, args['<url>'])
-	stream = client.get(track.stream_url, allow_redirects=False)
-	output = get_filename_for_track(args, client, track)
+    set_loglevel(args)
+    client = soundcloud.Client(client_id='7b4b979cb9f47830a7bb2441f6d784c3')
+    track = get_track_by_url(client, args['<url>'])
+    stream = client.get(track.stream_url, allow_redirects=False)
+    output = get_filename_for_track(args, client, track)
 
-	log.info('Downloading %s to %s' % (track.title, output))
-	
-	r = requests.get(stream.location)
-	log.debug('Response is %d bytes long', int(r.headers['Content-Length']))
+    log.info('Downloading %s to %s' % (track.title, output))
 
-	with open(output, 'w+') as f:
-		f.write(r.content)
+    r = requests.get(stream.location)
+    log.debug('Response is %d bytes long', int(r.headers['Content-Length']))
+
+    with open(output, 'w+') as f:
+        f.write(r.content)
+
 
 def get_track_by_url(client, url):
-	'''
-		Gets a track by the given Soundcloud URL.
+    '''
+            Gets a track by the given Soundcloud URL.
 
-		Example URL https://soundcloud.com/ovey/duck-sauce-barba-streisand-ovey-remix-clip
-	'''
-	try:
-		path = urlparse(url).path
-		tracks = client.get('/tracks', q=path)
+            Example URL https://soundcloud.com/ovey/duck-sauce-barba-streisand-ovey-remix-clip
+    '''
+    try:
+        path = urlparse(url).path
+        tracks = client.get('/tracks', q=path)
 
-		if tracks:
-			if len(tracks) > 1:
-				return tracks
-			else:
-				return tracks[0]
-		else:
-			raise ValueError('Track not found for URL %s' % url)
-	except Exception as e:
-		raise ValueError('Error obtaining track by url: %s' % str(e))
+        if tracks:
+            if len(tracks) > 1:
+                return tracks
+            else:
+                return tracks[0]
+        else:
+            raise ValueError('Track not found for URL %s' % url)
+    except Exception as e:
+        raise ValueError('Error obtaining track by url: %s' % str(e))
+
 
 def get_filename_for_track(args, client, track):
-	if not args['--output']:
-		title = track.title.lower().replace(' ', '_')
-		artist = track.user['permalink'].lower().replace(' ', '_')
+    if not args['--output']:
+        title = track.title.lower().replace(' ', '_')
+        artist = track.user['permalink'].lower().replace(' ', '_')
 
-		return '%s-%s.mp3' % (artist, title)
-	else:
-		return args['--output']
+        return '%s-%s.mp3' % (artist, title)
+    else:
+        return args['--output']
+
 
 def set_loglevel(args):
-	level = logging.INFO
-	if args.get('--verbose', None):
-		level = logging.DEBUG
+    level = logging.INFO
+    if args.get('--verbose', None):
+        level = logging.DEBUG
 
-	log.setLevel(level)
+    log.setLevel(level)
 
 if __name__ == '__main__':
-	args = docopt(__doc__)
-	main(args)
+    args = docopt(__doc__)
+    main(args)

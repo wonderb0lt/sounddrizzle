7491cc15b4b882376543db744a62cc56de715e88
Patrick Stegmann <code@patrick-stegmann.de>
2013-06-25 15:29:53 +0200

    Now metadata is also downloaded! Yay!

--- sounddrizzle.py
+++ sounddrizzle.py
@@ -5,7 +5,7 @@ Usage:
 	sounddrizzle.py [options] <url>
 
 --output <FILE> 			The output file (defaults to artist/title)
---verbose					More output!
+--verbose More output!
 '''
 
 from urlparse import urlparse
@@ -14,6 +14,8 @@ import logging
 from docopt import docopt
 import requests
 import soundcloud
+from mutagen.id3 import TPE1, TIT2, APIC
+from mutagen.mp3 import *
 
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(msg)s')
 log = logging.getLogger('SoundDrizzle')
@@ -21,18 +23,22 @@ log = logging.getLogger('SoundDrizzle')
 
 def main(args):
     set_loglevel(args)
+
     client = soundcloud.Client(client_id='7b4b979cb9f47830a7bb2441f6d784c3')
     track = get_track_by_url(client, args['<url>'])
     stream = client.get(track.stream_url, allow_redirects=False)
     output = get_filename_for_track(args, client, track)
 
     log.info('Downloading %s to %s' % (track.title, output))
+    # r = requests.get(stream.location)
+    #   log.debug('Response is %d bytes long', int(r.headers['Content-Length']))
+    #   with open(output, 'w+') as f:
+    #      f.write(r.content)
 
-    r = requests.get(stream.location)
-    log.debug('Response is %d bytes long', int(r.headers['Content-Length']))
 
-    with open(output, 'w+') as f:
-        f.write(r.content)
+    log.info('Adding metadata...')
+
+    enrich_file(output, track)
 
 
 def get_track_by_url(client, url):
@@ -66,8 +72,30 @@ def get_filename_for_track(args, client, track):
         return args['--output']
 
 
+def enrich_file(f, track):
+    mp3 = MP3(f)
+
+    mp3['TPE1'] = TPE1(encoding=3, text=track.user['username'])
+    mp3['TIT2'] = TIT2(encoding=3, text=track.title)
+
+    try:
+        cover_bytes = requests.get(track.artwork_url, stream=True).raw.read()
+        log.debug('Adding cover - length is %d bytes' % len(cover_bytes))
+        mp3.tags.add(APIC(
+        	encoding=3, 
+        	mime='image/jpeg', 
+        	type=3,
+        	desc='Front cover', 
+        	data=cover_bytes)
+       	)
+    except Exception as e:
+        print e
+
+    mp3.save()
+
+
 def set_loglevel(args):
-    level = logging.INFO
+    level = logging.DEBUG
     if args.get('--verbose', None):
         level = logging.DEBUG
 

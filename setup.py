79cf7bdd1c7b3a47433f357d5c1b89da093ef4de
Patrick Stegmann <code@patrick-stegmann.de>
2013-11-12 17:08:50 +0100

    Made the project distributable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='SoundDrizzle',
+    version='0.0.1',
+    description='An easy client to add SoundCloud tracks to your offline collection',
+    author='Patrick Stegmann (aka. @wonderb0lt)',
+    author_email='code@patrick-stegmann.de',
+    url='https://github.com/wonderb0lt/sounddrizzle',
+    packages=['sounddrizzle'],
+    requires=['mutagen', 'docopt'],
+    scripts=['scripts/drizzle']
+)

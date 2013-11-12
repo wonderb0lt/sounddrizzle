22035f268a7800391478bba30dc7a7ab11b035b2
Patrick Stegmann <code@patrick-stegmann.de>
2013-11-12 19:17:13 +0100

    Updated metadata

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ setup(
     version='0.0.1',
     description='An easy client to add SoundCloud tracks to your offline collection',
     long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        
+    ]
     license='LICENSE',
     author='Patrick Stegmann (aka. @wonderb0lt)',
     author_email='code@patrick-stegmann.de',
